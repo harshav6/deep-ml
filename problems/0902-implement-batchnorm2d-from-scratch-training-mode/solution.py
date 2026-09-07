b4b0a75e6479d13c2d94ea47ffe62ae14cee36e0
harshav6 <115707103+harshav6@users.noreply.github.com>
2026-09-07 03:12:58 +0000

    Solve 3 problems on 2026-09-07

---
+++ problems/0902-implement-batchnorm2d-from-scratch-training-mode/solution.py
@@ -0,0 +1,14 @@
+import torch
+
+def batchnorm2d(x, gamma, beta, eps=1e-5):
+
+    mean = x.mean(dim = (0, 2, 3), keepdim = True)
+    var = x.var(dim =(0, 2, 3), keepdim = True, unbiased = False)
+
+    x_norm = (x - mean) / torch.sqrt(var + eps)
+
+    gamma = gamma.reshape(1, -1, 1, 1)
+    beta = beta.reshape(1, -1, 1, 1)
+
+    return gamma * x_norm + beta
+    pass
